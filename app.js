ddd4ed2f321f1184cc7969c0f96fb318b7bd4751
jatiny17 <kumarjatinjt.1998@gmail.com>
2021-05-02 23:06:12 +0530

    Port modified

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const people = require("./routes/people.js");
 const login = require("./routes/login.js");
 
+const port = process.env.PORT || 5000;
+
 // app.use(express.static("./methods-public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -10,6 +12,6 @@ app.use(express.json());
 app.use("/login", login);
 app.use("/api/people", people);
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log("Listening on port 5000");
 })
